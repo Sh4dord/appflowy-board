b56bbb48c4797d59625abbc19a563de9be8b766a
Nathan ROLANDO <nathan.rolando@cloud-solutions.fr>
2025-08-22 17:11:36 +0200

    chore: review + task positions + minor bugs

--- lib/src/widgets/board_group/group.dart
+++ lib/src/widgets/board_group/group.dart
@@ -33,6 +33,7 @@ typedef AppFlowyBoardHeaderBuilder = Widget? Function(
 typedef AppFlowyBoardBuilder = Widget? Function(
   BuildContext context,
   AppFlowyGroupData groupData,
+  Widget child,
 );
 
 typedef AppFlowyBoardFooterBuilder = Widget Function(
@@ -144,44 +145,43 @@ class _AppFlowyBoardGroupState extends State<AppFlowyBoardGroup> {
           draggableTargetBuilder: PhantomDraggableBuilder(),
         );
 
-        final builderChild =
-            widget.builder?.call(context, widget.dataSource.groupData);
-        final child = builderChild ??
-            ReorderFlex(
-              key: ValueKey(widget.groupId),
-              dragStateStorage: widget.dragStateStorage,
-              dragTargetKeys: widget.dragTargetKeys,
-              scrollController: widget.scrollController,
-              config: widget.config,
-              onDragStarted: (index) {
-                widget.phantomController.groupStartDragging(widget.groupId);
-                widget.onDragStarted?.call(index);
-              },
-              onReorder: (fromIndex, toIndex) {
-                if (widget.phantomController.shouldReorder(widget.groupId)) {
-                  widget.onReorder(widget.groupId, fromIndex, toIndex);
-                  widget.phantomController.updateIndex(fromIndex, toIndex);
-                }
-              },
-              onDragEnded: () {
-                widget.phantomController.groupEndDragging(widget.groupId);
-                widget.onDragEnded?.call(widget.groupId);
-                widget.dataSource.debugPrint();
-              },
-              dataSource: widget.dataSource,
-              interceptor: interceptor,
-              reorderFlexAction: widget.reorderFlexAction,
-              children: children,
-            );
+        final reorderChild = SingleChildScrollView(
+          scrollDirection: widget.config.direction,
+          controller: widget.scrollController,
+          child: ReorderFlex(
+            key: ValueKey(widget.groupId),
+            dragStateStorage: widget.dragStateStorage,
+            dragTargetKeys: widget.dragTargetKeys,
+            scrollController: widget.scrollController,
+            config: widget.config,
+            onDragStarted: (index) {
+              widget.phantomController.groupStartDragging(widget.groupId);
+              widget.onDragStarted?.call(index);
+            },
+            onReorder: (fromIndex, toIndex) {
+              if (widget.phantomController.shouldReorder(widget.groupId)) {
+                widget.onReorder(widget.groupId, fromIndex, toIndex);
+                widget.phantomController.updateIndex(fromIndex, toIndex);
+              }
+            },
+            onDragEnded: () {
+              widget.phantomController.groupEndDragging(widget.groupId);
+              widget.onDragEnded?.call(widget.groupId);
+              widget.dataSource.debugPrint();
+            },
+            dataSource: widget.dataSource,
+            interceptor: interceptor,
+            reorderFlexAction: widget.reorderFlexAction,
+            children: children,
+          ),
+        );
+        final builderChild = widget.builder
+                ?.call(context, widget.dataSource.groupData, reorderChild) ??
+            reorderChild;
 
         final paddingWidget = Padding(
           padding: widget.bodyPadding,
-          child: builderChild ??
-              SingleChildScrollView(
-                scrollDirection: widget.config.direction,
-                controller: widget.scrollController,
-                child: child,
-              ),
+          child: builderChild,
         );
 
         final reorderWidget = widget.shrinkWrap
